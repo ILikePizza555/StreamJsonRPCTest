b3b737006dc3b9d4c2a6ac210898699e346373d8
Andrew Arnott <andrewarnott@gmail.com>
2019-12-01 21:30:35 -0700

    Reduce repro to absolute minimum
    No StreamJsonRpc required. Just AsSpan on an array that contains a derived runtime type.
    Mono fails because at runtime, the ParameterInfo[] is actually RuntimeParameterInfo[].

--- Program.cs
+++ Program.cs
@@ -1,25 +1,14 @@
 ﻿using System;
-using System.IO;
-using System.Threading.Tasks;
-using Nerdbank.Streams;
+using System.Reflection;
 
-namespace SJRTest
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static async Task Main(string[] args)
-        {
-            var (client, server) = SetUpClientAndServer();
-            var result = await client.RequestDataFromServerRPC("username");
-            Console.WriteLine($"Result: {result}");
-        }
-
-        static (Client, Server) SetUpClientAndServer()
-        {
-            (Stream, Stream) streams = FullDuplexStream.CreatePair();
-            var client = new Client(streams.Item1);
-            var server = new Server(streams.Item2);
-            return (client, server);
-        }
+        var m = typeof(Program).GetMethod(nameof(Foo), BindingFlags.Static | BindingFlags.NonPublic);
+        var a = m.GetParameters();
+        var s = a.AsSpan(0, 0);
     }
+
+    static void Foo() { }
 }
